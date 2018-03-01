44d1356d3073e5f558b5af3c4c9a008d6a08c242
Pavel Linhart <pavel.linhart@gmail.com>
2018-03-01 09:01:52 +0100

    can specify dimensions and fill color

--- src/SvgFilter.php
+++ src/SvgFilter.php
@@ -2,6 +2,7 @@
 
 namespace Nelson\Latte\Filters\SvgFilter;
 
+use DOMElement;
 use DOMDocument;
 use Nette\Caching\Cache;
 use Nette\Caching\IStorage;
@@ -49,15 +50,21 @@ class SvgFilter
 	 * @param  string $file
 	 * @return Html|void
 	 */
-	public function inline($file)
+	public function inline($file, $width = null, $height = null, $fill = null)
 	{
 		if (!empty($file)) {
 			$filepath = $this->assetsPath . $file;
 
-			$svg = $this->cache->call([$this, 'getSvg'], $filepath);
+			// Has to be string because of cache
+			$rawString = $this->cache->call([$this, 'getSvg'], $filepath);
+			$document = $this->getDOMDocument($rawString);
+			$element = $this->getSVGElement($document);
 
-			if (!empty($svg)) {
-				return Html::el()->setHtml($svg);
+			$element = $this->applyDimensions($element, $width, $height);
+			$element = $this->applyFill($element, $fill);
+
+			if (!empty($document)) {
+				return Html::el()->setHtml($document->saveHTML());
 			}
 		}
 	}
@@ -71,13 +78,71 @@ class SvgFilter
 	{
 		$content = FileSystem::read($filepath);
 
-		$doc = new DOMDocument();
-		$doc->loadXML($content);
-		$svgElement = $doc->getElementsByTagName('svg')->item(0);
+		$document = $this->getDOMDocument($content);
+		$element = $this->getSVGElement($document);
 
-		$doc2 = new DOMDocument();
-		$doc2->appendChild($doc2->importNode($svgElement, true));
+		$result = new DOMDocument();
+		$result->appendChild($result->importNode($element, true));
 
-		return $doc2->saveHtml();
+		return $result->saveHTML();
+	}
+
+
+	/**
+	 * @param  DOMElement $element
+	 * @param  string     $width
+	 * @param  string     $height
+	 * @return DOMElement
+	 */
+	protected function applyDimensions(DOMElement $element, $width, $height)
+	{
+		if (empty($width) OR empty($height)) {
+			return $element;
+		}
+
+		$element->setAttribute('width', $width);
+		$element->setAttribute('height', $height);
+
+		return $element;
+	}
+
+
+	/**
+	 * @param  DOMElement $element
+	 * @param  string     $fill
+	 * @return DOMElement
+	 */
+	protected function applyFill(DOMElement $element, $fill)
+	{
+		if (empty($fill)) {
+			return $element;
+		}
+
+		$element->setAttribute('fill', $fill);
+
+		return $element;
+	}
+
+
+	/**
+	 * @param  string $svg
+	 * @return DOMDocument
+	 */
+	protected function getDOMDocument($svg)
+	{
+		$document = new DOMDocument();
+		$document->loadXML($svg);
+
+		return $document;
+	}
+
+
+	/**
+	 * @param  DOMDocument $document
+	 * @return DOMElement
+	 */
+	protected function getSVGElement(DOMDocument $document)
+	{
+		return $document->getElementsByTagName('svg')->item(0);
 	}
 }
